c22616977bc147a78406104289188edfea096c7f
JP-code33 <pateljhenil@gmail.com>
2026-07-18 13:40:27 -0400

    music library

--- Jarvis V2.py
+++ Jarvis V2.py
@@ -1,27 +1,46 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import musicLibrary
 
 recognizaer = sr.Recognizer()
-ttsx = pyttsx3.init()
+engine = pyttsx3.init()
 
 def speak(text):
-    ttsx.say(text)
-    ttsx.runAndWait()
+    engine.say(text)
+    engine.runAndWait()
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif c.lower().startwith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
 
 if __name__ == "__main__":
-    speak("Intitializing Jarvis....")
+    speak("Initializing Jarvis....")
     while True:
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Lsitening...")
-            audio = r.listen(source, timeout=2)
-        
 
+        print("recongnizing...")        
         try:
-            command = r.recognize_google(audio)
-            print (command)
-        except sr.UnknownValueError:
-            print("Sphinx could not understand audio")
-        except sr.RequestError as e:
-            print("Sphinx error; {0}".format(e))
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=3, phrase_time_limit=3)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                engine.say("Yes Sir")
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+        except Exception as e:
+            print("Error; {0}".format(e))
