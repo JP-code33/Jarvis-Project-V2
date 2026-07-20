d945c99a35093d2dbc5785ffe099fd4e5863b4ae
JP-code33 <pateljhenil@gmail.com>
2026-07-20 17:58:54 -0400

    Update Jarvis V2.py

--- Jarvis V2.py
+++ Jarvis V2.py
@@ -1,14 +1,30 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import os
+from dotenv import load_dotenv
+from google import genai
 
-
+load_dotenv
 recognizaer = sr.Recognizer()
 engine = pyttsx3.init()
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
+def aiCommand(command):
+    try:
+        secret_key = os.getenv("GEMINI_API_KEY")
+        client = genai.Client(api_key=secret_key)
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=f"System instruction: You are a virtual assistant named Jarvis skilled in general tasks. Respond in one short sentence: {command}"
+        )
+        return response.text
+    except Exception as e:
+        return "My network paths are currently offline, Sir!"
 
 def processCommand(c):
     if "open google" in c.lower():
@@ -17,7 +33,14 @@ def processCommand(c):
         webbrowser.open("https://facebook.com")
     elif "open youtube" in c.lower():
         webbrowser.open("https://youtube.com")
-    
+    else:
+        engine.say("Consulting artiflicial intelligence core...")
+        engine.runAndWait
+
+        ai_reply = aiCommand(c)
+
+        engine.say(ai_reply)
+        engine.runAndWait
  
 
 if __name__ == "__main__":
